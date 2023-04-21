6373d0cab9d3c524a275ab612b00d009e02b5a5c
Jhony Alife <jhonyalife@gmail.com>
2023-04-21 18:21:02 -0300

    aplicando modulo de cadastro para integrar com back

---
+++ js/cadastrar.js
@@ -0,0 +1,46 @@
+const formulario = document.querySelector('.form-registe');
+const Inome = document.querySelector('.name-registe');
+const Iemail = document.querySelector('.email-registe');
+const Isenha = document.querySelector('.password-registe');
+
+
+function cadastrar() {
+    fetch('http://localhost:8080/usuarios', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: Inome.value,
+            email: Iemail.value,
+            senha: Isenha.value,
+            classeUsuario: 2
+        }) // Converte os dados para JSON e envia no corpo da requisição
+    })
+        .then(response => {
+            if (response.ok) {
+                // Se a resposta foi bem sucedida, manipula os dados recebidos
+                console.log("Usuário criado com sucesso!");
+            } else {
+                // Se a resposta foi mal sucedida, lança um erro
+                throw new Error("Não foi possível criar o usuário.");
+            }
+        })
+        .catch(error => {
+            // Trata os erros
+            console.error(error);
+        });
+}
+
+function limpar() {
+    Inome.value = "";
+    Iemail.value = "";
+    Isenha.value = "";
+
+}
+
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+    cadastrar();
+    limpar();
+})
